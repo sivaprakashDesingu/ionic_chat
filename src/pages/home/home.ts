022f0f09b74fc9bcda73fba0a6bc33c97f297a79
Sivaprakash Desingu <10977339+sivaprakashDesingu@users.noreply.github.com>
2018-09-30 19:06:37 +0530

    ionic chat app dev stage
    ionic chat app dev stage

---
+++ src/pages/home/home.ts
@@ -0,0 +1,79 @@
+import { Component, OnInit } from '@angular/core';
+import { NavController,ToastController  } from 'ionic-angular';
+
+import { URLSearchParams } from '@angular/http';
+//import { Router } from "@angular/router";
+
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+import { CookieService } from 'ngx-cookie-service';
+import { AppService } from '../service/appService';
+import { DashPage } from '../dash/dash';
+import { RegisterPage } from '../register/register';
+import { ForgotPage } from '../forgot/forgot';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage implements OnInit {
+
+
+  // constructor(public navCtrl: NavController) {
+
+  // }
+
+  private ulst;
+  private uid;
+  private upswd;
+
+  constructor(private appService: AppService, public navCtrl: NavController, private http: HttpClient, private cookie: CookieService) { }
+  
+  
+  
+  getUser() {
+    this.http.get(this.appService.API_ENDPOINT + 'isValiduser', {
+      params: {
+        uid: this.uid
+      }
+    })
+      .subscribe(data => {
+        this.ulst = data;
+        for (var i = 0; i < this.ulst.length; i++) {
+          if (this.upswd == this.ulst[i].pswd && this.uid == this.ulst[i].uid) {
+            this.cookie.set('luser', this.uid);
+            this.cookie.set('luname', this.ulst[i].uname);
+            this.navCtrl.push(DashPage);
+            //this.push.navigate(['/dash']);
+          } else {
+              this.appService.presentToast("Please give me a Valid input");
+          }
+        }
+        if(this.ulst.length==0){
+          this.appService.presentToast("Please give me a Valid input");
+        }
+      },
+      err => console.log(err),
+      () => console.log());
+  }
+  isLoggedIn() {
+    this.getUser();
+   // this.navCtrl.push(DashPage);
+
+
+  }
+  nextPage(pageName){
+    if(pageName=="fp")
+      this.navCtrl.push(ForgotPage);
+    else
+      this.navCtrl.push(RegisterPage);
+
+  }
+  ngOnInit() {
+
+
+  }
+
+}
---
+++ src/pages/service/appService.ts
@@ -0,0 +1,38 @@
+import { Injectable, Component } from '@angular/core';
+import { HomePage } from '../home/home';
+import { HttpClient } from '@angular/common/http';
+import 'rxjs/add/operator/map'
+import { ToastController } from 'ionic-angular';
+import { NgModule } from '@angular/core';
+
+@Component({
+  providers: [HomePage]
+})
+
+@NgModule({
+})
+
+@Injectable()
+export class AppService {
+
+  constructor(private toastCtrl: ToastController) { }
+  public API_ENDPOINT = 'http://localhost:3000/';
+  //public  API_ENDPOINT='http://ec2-18-224-82-32.us-east-2.compute.amazonaws.com:3000/';
+
+  presentToast(txt) {
+    let toast = this.toastCtrl.create({
+      message: txt,
+      duration: 3000,
+      position: 'bottom'
+    });
+
+    toast.onDidDismiss(() => {
+      console.log('Dismissed toast');
+    });
+
+    toast.present();
+  }
+
+  
+}
+
